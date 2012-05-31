a131406940771f84725b78b447e8a5b50ce792e7
pau fraces <pau.fraces@gmail.com>
2012-05-31 03:46:24 +0000

    se separan los tests en diferentes casos

--- testInternals.js
+++ testInternals.js
@@ -1,80 +1,88 @@
-testCase ("js internals test", {
-    "test new var is undefined": function() {
+testCase ("tipo de datos", {
+    "new var is undefined": function() {
         var foo;
         assertUndefined(foo);
     },
-    "test empty brackets is object": function() {
+    "empty brackets is object": function() {
         assertObject({});
     },
-    "test digit is number": function() {
+    "digit is number": function() {
         assertNumber(2);
     },
-    "test digit.digit is number": function() {
+    "digit.digit is number": function() {
         assertNumber(0.5);
     },
-    "test quoted text is string": function() {
+    "quoted text is string": function() {
         assertString("foo");
     },
-    "test empty annonimous function is function": function() {
+    "empty annonimous function is function": function() {
         assertFunction(function() {});
     },
-    "test empty named function is function": function() {
+    "empty named function is function": function() {
         assertFunction(function foo() {});
     },
-    "test invoking empty annonymous function is undefined": function() {
+    "invoking empty annonymous function is undefined": function() {
         assertUndefined(function() {}());
     },
-    "test invoking empty named function is undefined": function() {
+    "invoking empty named function is undefined": function() {
         assertUndefined(function foo() {}());
     },
-    "test invoking empty annonymous function wrapped in brackets is undefined":
+    "invoking empty annonymous function wrapped in brackets is undefined":
     function() {
         assertUndefined((function() {}()));
     },
-    "test invoking function which returns number is number": function() {
+    "invoking function which returns number is number": function() {
         assertNumber(function() {return 2;}());
-    },
-    
-    // vamos a hacer unos tests para demostrar las diferencias entre los
-    // operadores logicos (devuelven true o false) !== y !=
-    "test == da true para mismo valor y mismo tipo": function() {
+    }
+});
+  
+/**
+* vamos a hacer unos tests para demostrar las diferencias entre los
+* operadores logicos (devuelven true o false) !== y !=
+*/
+
+testCase("comparaciones estrictas", {
+    "== da true para mismo valor y mismo tipo": function() {
         assertTrue(3 == 3);
     },
-    "test == da true para mismo valor y diferente tipo": function() {
+    "== da true para mismo valor y diferente tipo": function() {
         assertTrue(3 == "3");
     },
-    "test === da true para mismo valor y mismo tipo": function() {
+    "=== da true para mismo valor y mismo tipo": function() {
         assertTrue(3 === 3);
     },
-    "test === da false para mismo valor y diferente tipo": function() {
+    "=== da false para mismo valor y diferente tipo": function() {
         assertFalse(3 === "3");
-    },
+    }
+});
     
-    /**
-     * vamos ha probar algunas propiedades de los objetos
-     * el acceso a traves de dot notation: objeto.propiedad
-     * el acceso a traves de corchetes: objeto[propiedad]
-    **/
-    "test object with property": function() {
+/**
+* vamos ha probar algunas propiedades de los objetos
+* el acceso a traves de dot notation: objeto.propiedad
+* el acceso a traves de corchetes: objeto[propiedad]
+*/
+
+testCase ("objetos y propiedades", {
+    "object with property": function() {
         var foo = {"name": "john doe"};
         assertObject(foo);
     },
-    "test accessing object property by dot notation": function() {
+    "accessing object property by dot notation": function() {
         var name = "john doe";
         var foo = {"name": name};
         assertEquals(name, foo.name);
     },
-    "test accessing object property by corcheting" : function() {
+    "accessing object property by corcheting" : function() {
         var name = "john doe";
         var foo = {"name" : name};
         assertEquals(name, foo["name"]);
     },
-    "test accessing object property by corcheting with var" : function() {
+    "accessing object property by corcheting with var" : function() {
         var x = "name";
         var foo = {"name" : "john doe"};
         assertEquals(foo[x], foo["name"]);
     },
-    "test accessing object property with string var": function() {
+    "accessing object property with string var": function() {
         var objName = "john doe";
         var objAge = 22;
         var obj = {
@@ -83,7 +91,7 @@ testCase ("js internals test", {
         };
         assertEquals(obj["name"], "john doe");
     },
-    "test accessing object property with var": function() {
+    "accessing object property with var": function() {
         var objName = "john doe";
         var objAge = 22;
         var obj = {
@@ -92,64 +100,76 @@ testCase ("js internals test", {
         };
         var name = "age";
         assertEquals(obj[name], objAge);
-    },
-    
-    //Uso de caracteres unicode como variables
-    "test var ASCII como variable" : function() {
+    }
+});
+
+/**
+* Uso de caracteres unicode como variables
+*/
+
+testCase("nombres unicode", {
+    "var ASCII como variable" : function() {
         var obj = {"€": 22};
         assertEquals(obj["€"], 22);
-    },
+    }
+});
     
-    /**
-     * probar como funcionan los operadores de incremento:
-     * - existe el incremento como prefijo?
-     * - existe el incremento como postfijo?
-     * - tienen diferencias como en c?
-     */
-    "test pre-incremento incrementa antes" : function() {
+/**
+* probar como funcionan los operadores de incremento:
+* - existe el incremento como prefijo?
+* - existe el incremento como postfijo?
+* - tienen diferencias como en c?
+*/
+
+testCase("operadores de incremento", {
+    "pre-incremento incrementa antes" : function() {
         var a = 0;
         assertEquals(1, ++a); 
     },
-    "test post-incremento no incrementa antes" : function() {
+    "post-incremento no incrementa antes" : function() {
         var a = 0;
         assertNotEquals(1, a++);
     },
-    "test post-incremento incrementa después" : function() {
+    "post-incremento incrementa después" : function() {
         var a = 0;
         a++;
         assertEquals(1, a);
-    },
+    }
+});
     
-    /**
-     * he leido que si se comparan dos objetos iguales con === da false
-     * voy a hacer varios tests para comprobar este comportamiento
-     * y buscar informacion para descubrir porque
-     * http://code.google.com/p/js-test-driver/issues/detail?id=119
-     */
-     
-    "test dos objetos vacios no son estrictamente iguales": function () {
+/**
+* he leido que si se comparan dos objetos iguales con === da false
+* voy a hacer varios tests para comprobar este comportamiento
+* y buscar informacion para descubrir porque
+* http://code.google.com/p/js-test-driver/issues/detail?id=119
+*/
+
+testCase("comparando objetos", {
+    "dos objetos vacios no son estrictamente iguales": function () {
         assertNotSame({}, {});
     },
-    "test dos objectos vacios no son iguales": function () {
+    "dos objectos vacios no son iguales": function () {
         assertNotEquals({}, {});
     },
-    "test dos objetos vacios copiados por variable son estrictamente iguales":
+    "dos objetos vacios copiados por variable son estrictamente iguales":
     function () {
         var a = {};
         var b = a;
         assertSame(a, b);
     },
-    "test dos objetos vacios en variables no son iguales": function () {
+    "dos objetos vacios en variables no son iguales": function () {
         var a = {};
         var b = {};
         assertNotEquals(a, b);
-    },
+    }
+});
      
-    /**
-     * Scope
-     */
-      
-    "test function scope": function() {
+/**
+* Scope
+*/
+    
+testCase("scope", {
+    "function scope": function() {
         var x = 0,
             y = (function() {
                 var x = 1;
@@ -158,7 +178,7 @@ testCase ("js internals test", {
         
         assertNotEquals(x, y)
     },
-    "test no braces scope": function() {
+    "no braces scope": function() {
         var x = 0;
         
         if (true) {
@@ -166,33 +186,37 @@ testCase ("js internals test", {
         }
         
         assertEquals(x, 1); 
-    },
+    }
+});
      
-    /**
-     * This object
-     */
-      
-    "test x is undefined in this scope": function() {
+/**
+* This object
+*/
+
+testCase("el objeto this", {
+    "x is undefined in this scope": function() {
         assertFalse(this.x);
     },
-    "test without var, not using this scope": function() {
+    "without var, not using this scope": function() {
         x = 0;
         assertFalse(this.x);
     },
-    "test using var not means using this scope": function() {
+    "using var not means using this scope": function() {
         var x = 1;
         assertFalse(this.x);
     },
-    "test without var, variable still defined": function() {
+    "without var, variable still defined": function() {
         x = 0;
         assertEquals(x, 0);
-    },
+    }
+});
     
-    /**
-     * That: a copy of this object
-     */
+/**
+* That: a copy of this object
+*/
     
-    "test using var not means using that scope": function() {
+testCase("copiando this en that", {
+    "using var not means using that scope": function() {
         var that = this;
         var x = 0;
         assertFalse(that.x);
